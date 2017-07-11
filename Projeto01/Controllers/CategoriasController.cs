e450cd90bf8fac17c29b07fd031151118e861ca6
Fabio Zanardo <shadomhp@gmail.com>
2017-07-11 01:33:13 -0300

    Add Initial Commit

---
+++ Projeto01/Controllers/CategoriasController.cs
@@ -0,0 +1,54 @@
+﻿using Projeto01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto01.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria()
+            {
+                CategoriaID = 1,
+                Nome = "Notebooks"
+            },
+            new Categoria()
+            {
+                CategoriaID = 2,
+                Nome = "Monitores"
+            },
+            new Categoria()
+            {
+                CategoriaID = 3,
+                Nome = "Impressoras"
+            },
+            new Categoria()
+            {
+                CategoriaID = 4,
+                Nome = "Mouses"
+            },
+            new Categoria()
+            {
+                CategoriaID = 5,
+                Nome = "Desktops"
+            }
+
+        };
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+
+        //GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
