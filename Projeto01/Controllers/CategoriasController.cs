4578497f8a80945c10ed412f978b1e29bfd77106
Fabio Zanardo <shadomhp@gmail.com>
2017-07-11 23:47:45 -0300

    Finalização do Primeiro Capitulo

--- Projeto01/Controllers/CategoriasController.cs
+++ Projeto01/Controllers/CategoriasController.cs
@@ -50,5 +50,50 @@ namespace Projeto01.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create (Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaID = categorias.Select(m => m.CategoriaID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            categorias[categorias.IndexOf(categorias.Where(c => c.CategoriaID == categoria.CategoriaID).First())] = categoria;
+            
+            // Outro Jeito de ser feito. Removendo e Adic
+            // categorias.Remove(categorias.Where(c => c.CategoriaID == categoria.CategoriaID).First());
+            // categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult Details(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaID == id).First());
+        }
+
+        public ActionResult Delete(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(c => c.CategoriaID == categoria.CategoriaID).First());
+            return RedirectToAction("Index");
+        }
+
     }
 }
