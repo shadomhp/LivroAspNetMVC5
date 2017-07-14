c8f6e8d6040e8aed52811bb50c9d3266263e3d18
Fabio Zanardo <shadomhp@gmail.com>
2017-07-13 21:29:14 -0300

    add Fabricantes Create e Edit via EF

---
+++ Projeto01/Controllers/FabricantesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto01.Contexts;
+using Projeto01.Models;
+using System.Net;
+
+namespace Projeto01.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+
+        //Responsável pela geração da View Inserção no banco
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);  // Gera codigos de erro HTTP
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+
+
+    }
+}
